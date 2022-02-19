3ea1f4b6cabd4c86d622f32d08c1ff03f0c1c8fb
mh8 <moazzemhb@gmail.com>
2022-02-19 13:47:45 +0600

    DB

---
+++ database/factories/PeopleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PeopleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'id_no' => $this->faker->ean8(),
+            'dob' => $this->faker->date(),
+            'office' => $this->faker->word,
+            'registered' => rand(0, 1),
+        ];
+    }
+}
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,12 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="min-w-full divide-y divide-gray-200">
+                    <table class="w-full border-r border-b">
                         <thead>
                             <tr>
-                                <th>Sl</th>
-                                <th>Vaccine Name</th>
-                                <th>Minimum Age</th>
+                                <th class="border-l border-t px-1 py-2 text-left">Sl</th>
+                                <th class="border-l border-t px-1 py-2 text-center"> Vaccine Name</th>
+                                <th class="border-l border-t px-1 py-2 text-center">Minimum Age</th>
+                                <th class="border-l border-t px-1 py-2 text-center">Actions</th>
                             </tr>
 
                         </thead>
@@ -22,9 +23,10 @@
                             @foreach($categories as
                             $key => $category)
                             <tr>
-                                <td>{{ $key+1 }}</td>
-                                <td>{{ $category->name }}</td>
-                                <td>{{ $category->minimum_age }}</td>
+                                <td class="border-l border-t px-1 py-2 text-left">{{ $key+1 }}</td>
+                                <td class="border-l border-t px-1 py-2 text-center">{{ $category->name }}</td>
+                                <td class="border-l border-t px-1 py-2 text-center">{{ $category->minimum_age }}</td>
+                                <td class="border-l border-t px-1 py-2 text-center"><a href="{{ route('categories.edit', $category->id) }}" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow">Edit</a></td>
                             </tr>
                             @endforeach
 
